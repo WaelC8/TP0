fb4d8418034c60a498330f8a13ad33e0523b4c19
WaelC8 <waelcherkaoui@icloud.com>
2024-09-05 16:58:34 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
-rayon = 4
-
+rayon = 4 
+Aire = aire_cercle * rayon ** 2
+print(f"L'aire du cercle avec un rayon de {rayon} est {aire:.2f}")
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+Cherkaoui WAEL
